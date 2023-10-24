c05c87a807599a50280fcf504b389b19ff8e3d28
Raphaël <raclot.raphael@gmail.com>
2023-10-24 15:32:52 +0200

    Updates

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import greenit from "./greenit.png";
+import ethical_hacking from "./ethical_hacking.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import C from "./tech/c.png";
+import webflow from "./tech/webflow.png";
+import php from "./tech/php.svg";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import git from "./tech/git.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import python from "./tech/python.png";
+import tailwind from "./tech/tailwind.png";
+import wordpress from "./tech/wordpress.png";
+import threejs from "./tech/threejs.svg";
+
+import coda from "./company/coda.png";
+import scse from "./company/scse.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import mari from "../pics/C.png";
+import maxime from "../pics/maxime.jpeg";
+
+export {
+  logo,
+  backend,
+  greenit,
+  ethical_hacking,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  C,
+  webflow,
+  php,
+  html,
+  javascript,
+  git,
+  nodejs,
+  reactjs,
+  python,
+  tailwind,
+  wordpress,
+  threejs,
+  coda,
+  scse,
+  carrent,
+  jobit,
+  tripguide,
+  mari,
+  maxime,
+};
